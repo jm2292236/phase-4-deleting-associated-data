38874c5c633d3c7e282dad575ded186884fed91a
Jaime Montes de Oca <jaime0422@live.com>
2022-08-28 11:42:38 -0700

    Completed Assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,15 +1,21 @@
 class DogHousesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house, include: :reviews
-  end
+    def show
+        dog_house = DogHouse.find(params[:id])
+        render json: dog_house, include: :reviews
+    end
 
-  private
+    def destroy
+        dog_house = DogHouse.find(params[:id])
+        dog_house.destroy
+        head :no_content
+    end
 
-  def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
-  end
+    private
+
+    def render_not_found_response
+        render json: { error: "Dog house not found" }, status: :not_found
+    end
 
 end
